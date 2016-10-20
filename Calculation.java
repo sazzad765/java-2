d3088ae1824cdf4a1548679c256684b52eccd71b
sazzad765 <shfahad.920@gmail.com>
2016-10-21 02:09:32 +0600

    Add files via upload

---
+++ Calculation.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculation;
+
+/**
+ *
+ * @author sazzad
+ */
+public class Calculation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       double a=(9.5*4.5-2.5*3)/(45.5-3.5);
+        System.out.println("result="+a);
+    }
+    
+}
