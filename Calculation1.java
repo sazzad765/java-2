e6d9021297150d9fc4efa5c0cf412e44402fbadb
sazzad765 <shfahad.920@gmail.com>
2016-10-21 02:10:03 +0600

    Add files via upload

---
+++ Calculation1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculation1;
+
+/**
+ *
+ * @author sazzad
+ */
+public class Calculation1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=1+2+3+4+5+6+7+8+9;
+        System.out.println("result="+a);
+    }
+    
+}
